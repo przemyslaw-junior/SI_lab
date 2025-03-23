5da6e0f80522bb2bd69d35ac33709951a7b9c8d8
przem <przemek8319@gmail.com>
2025-03-23 14:04:45 +0100

    add lab_2 wstawienie if stopu w BFS i DFS

--- lab_2/lab_2.py
+++ lab_2/lab_2.py
@@ -12,6 +12,14 @@ def is_safe(queens, x, y):
             return False
     return True
 
+def is_safe_solution(queens):
+    for i in range(len(queens)):
+        for j in range(i +1, len(queens)):
+            qx1, qy1 = queens[i]
+            qx2, qy2 = queens[j]
+            if qx1 == qx2 or qy1 == qy2 or abs(qx1 - qx2) == abs(qy1 - qy2):
+                return False
+    return True
 
 checked_state = 0
 
@@ -55,7 +63,7 @@ def bfs_n_hetman(N):
 
         # print(f"bfs sprawdzenie stanu {checked_state_bfs}: {state}")
         # jezeli mamy N hetmanów, znaleziono pełne rozwiązanie
-        if len(state) == N:
+        if len(state) == N and is_safe_solution(state):
             end_time = time.perf_counter() - start_time
             open_states = len(Open)
             return state, end_time, checked_state, open_states
@@ -91,7 +99,8 @@ def dfs_n_hetman(N):
         checked_state += 1
         # print(f"dsf sprawdzenie stanu: {checked_state_dfs}: {state}")
         # jezeli mamy N hetmanów, znaleziono pełne rozwiązanie
-        if len(state) == N:
+        if len(state) == N and  is_safe_solution(state):
+
             end_time = time.perf_counter() - start_time
             open_states = len(Open)
             return state, end_time, checked_state, open_states
